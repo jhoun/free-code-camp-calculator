e24b43525acd45efaa22bb53c336dab6e898f452
jhoun <jayhoun@gmail.com>
2017-08-22 13:00:51 -0700

    added + functionality

--- app.js
+++ app.js
@@ -1,6 +1,64 @@
+var calculatorModule = (function() {
+  var memory = "";
+  var total = "";
+  var operator = "";
+
+  var isValid = function(num){
+    if (typeof num === 'number'){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  return {
+    load : function(num) {
+      total += num;
+    },
+    add: function(num){
+      total = memory + num;
+    },
+    saveOperator: function(val){
+      operator = val;
+    },
+    recallOperator: function(val){
+      return operator;
+    },
+    getTotal: function(){
+      return total;
+    },
+    clearTotal: function(){
+      total = "";
+    },
+    saveMemory: function(){
+      memory = parseFloat(total);
+    },
+    recallMemory: function(){
+      return memory;
+    }
+  };
+}());
+
 $(document).ready(function() {
   $(".btn").click(function(e) {
-    console.log('e: ', e);
-    $(".output").text(e.currentTarget.innerHTML);
+    var value = e.currentTarget.innerHTML;
+    var firstClick = "";
+    var myCalc = calculatorModule;
+    var operators = ["+", "-", "x", "÷"];
+    if (operators.indexOf(value) > -1){
+      myCalc.saveMemory()
+      myCalc.saveOperator(value);
+      myCalc.clearTotal();
+    } else if (value === "="){
+      if (myCalc.recallOperator() === "+"){
+        myCalc.add(parseFloat(myCalc.getTotal()));
+      }
+      myCalc.saveMemory();
+    } else {
+      myCalc.load(value);
+      firstClick = true;
+    }
+    $(".output").text(myCalc.getTotal());
   })
 });
+
